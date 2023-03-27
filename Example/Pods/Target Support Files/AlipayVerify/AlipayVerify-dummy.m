20be7d8cbb63f16457cc13522ec31d2dd97e905e
纵昂 <zongang15@yahoo.com>
2023-03-27 08:27:26 +0800

    first commit

---
+++ Example/Pods/Target Support Files/AlipayVerify/AlipayVerify-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AlipayVerify : NSObject
+@end
+@implementation PodsDummy_AlipayVerify
+@end
---
+++ Example/Pods/Target Support Files/Pods-AlipayVerify_Tests/Pods-AlipayVerify_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlipayVerify_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AlipayVerify_Tests
+@end
